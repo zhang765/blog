6a11a737cf4807a59b72fb1b49785099d251a752
PC-20170621GLWX\Administrator <zhangkai_zhi@qq.com>
2017-10-20 14:59:11 +0800

    admin add

---
+++ database/migrations/2017_10_20_142851_create_logs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->mediumInteger('uid')->comment('用户名');
+            $table->tinyInteger('action')->comment('动作');
+            $table->string('model')->comment('模块');
+            $table->mediumInteger('this_id')->comment('操作内容的id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
